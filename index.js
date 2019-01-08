9fbe51807374fa2d2f77067cf3ef7ec6da18fbc2
Barbara Washington <barbara.m.washington@gmail.com>
2019-01-08 01:31:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,14 +21,15 @@ function distanceTravelledInFeet(start, end) {
 }
 
 function calculatesFarePrice(start, destination) {
-    const distance = distanceTravelledInFeet(start, destination);
-    if (distance <= 400) {
-        return 0;
-    } else if (distance > 2500) {
-        return "cannot travel that far";
-    } else if (distance > 2000) {
-        return 25;
-    } else {
-        return distance * 0.02;
-    }
+  const distance = distanceTravelledInFeet(start, destination);
+
+  if (distance <= 400) {
+    return 0;
+  } else if (distance > 400 && distance <= 2000) {
+    return (distance - 400) * .02
+  } else if (distance > 2000 && distance < 2500) {
+    return 25;
+  } else {
+    return 'cannot travel that far';
+  }
 }
