8fcb4bada715540f014016f915974eec8a1e7d51
Barbara Washington <barbara.m.washington@gmail.com>
2019-01-08 01:08:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // Code your solution in this file!
 
-function distanceFromHqInBlocks(street) {
-  if (street >= 42) {
-    return street - 42;
+function distanceFromHqInBlocks(blocks) {
+  if (blocks >= 42) {
+    return blocks - 42;
   } else {
-    return 42 - street;
+    return 42 - blocks;
   };
 };
